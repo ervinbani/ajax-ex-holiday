5325e59fd1428c11e6c3889a9063bf236d2821b0
ervinbani <bani.ervin@gmail.com>
2018-04-18 18:10:47 +0200

    ex holiday

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 for(var i=2000; i<2018;i++){
-    $('.container').children('#year').append("<option class=option>"+i+"</option>");
+    $('.container').children('#year').append("<option class=newyear value="+i+">"+i+"</option>");
 }
 for(var i=1; i<13;i++){
-    $('.container').children('#month').append("<option class=option>"+i+"</option>");
+    $('.container').children('#month').append("<option class=option value="+i+">"+i+"</option>");
 }
 
 $(document).ready(function(){
 
-      $(document).on('click', '#btn', function(){
-        alert('ciao');
+      $(document).on('click', $('#btn'), function(){
+        var state=$('.container').children('#country').children('.option:selected');
+        var correntyear=$('.container').children('#year').children('.newyear:selected');
+        thisState=state.val();
+        thisYear=correntyear.val();
+
+          console.log(thisState);
+
+
+
+
           $.ajax({
               url:'https://holidayapi.com/v1/holidays',
               method:'GET',
               data:{
                   key:'2c6965c6-0570-42d7-b76a-8e54095558ef',
-                  cauntry:'IT',
-                  year:'2017',
+                  country:thisState,
+                  year:thisYear,
 
               },
 
